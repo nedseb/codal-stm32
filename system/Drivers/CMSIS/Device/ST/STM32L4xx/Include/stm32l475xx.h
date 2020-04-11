ea08b71c533c4cdcf4be3f2006be24e617c24eaf
Sébastien NEDJAR <sebastien@nedjar.com>
2020-04-11 15:16:32 +0200

    Ajout de CMSIS

--- system/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h
+++ system/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h
@@ -162,8 +162,7 @@ typedef enum
   * @}
   */
 
-#include "
-core_cm4.h"             /* Cortex-M4 processor and core peripherals */
+#include "core_cm4.h"             /* Cortex-M4 processor and core peripherals */
 #include "system_stm32l4xx.h"
 #include <stdint.h>
 
