c6015db9aec6b51d6739a46d901172f67f87589c
Sébastien NEDJAR <sebastien@nedjar.com>
2020-04-13 14:22:33 +0200

    Add platform includes

---
+++ inc/platform_includes.h
@@ -0,0 +1,13 @@
+#ifndef PLATFORM_INCLUDES
+#define PLATFORM_INCLUDES
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <math.h>
+
+#define PROCESSOR_WORD_TYPE uint32_t
+
+#endif /*PLATFORM_INCLUDES*/
