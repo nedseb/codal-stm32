0dbb19b3545dc1c9628c6c105e2badeec33a931e
Jonathan BAUDIN <jonathan.baudin@zeugma.cc>
2021-01-12 16:21:53 +0100

    First framebuffer version

---
+++ inc/drivers/frameBuffer.h
@@ -0,0 +1,43 @@
+#ifndef __FRAMEBUFFER_H__
+#define __FRAMEBUFFER_H__
+
+#include <cstdint>
+#include <string>
+
+namespace codal{
+
+    class FrameBuffer {
+
+        public:
+            enum Format{
+                MONO_VLSB,
+                // MONO_HLSB,
+                // MONO_HMSB,
+                // RGB565,
+                // GS2_HMSB,
+                // GS4_HMSB,
+                // GS8
+            };
+
+            FrameBuffer( unsigned widthPixel, unsigned heightPixel, Format format);
+            ~FrameBuffer();
+
+            void fill(uint16_t value);
+            void drawPixel( unsigned x, unsigned y, uint16_t color);
+            void drawChar(char c, unsigned x, unsigned y, uint16_t color);
+            void drawText(std::string str, unsigned x, unsigned y, uint16_t color);
+
+        protected:
+            uint8_t * buffer;
+
+        private:
+            unsigned width;
+            unsigned height;
+            unsigned bufferSize;
+            Format format;
+
+            void drawPixel1Bit( unsigned x, unsigned y, uint8_t color );
+    };
+}
+
+#endif
