7da55aee588b89a59700b563525881905f68286e
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-01-25 17:01:26 +0100

    Implement new driver I2C

--- inc/STM32I2C.h
+++ inc/STM32I2C.h
@@ -23,13 +23,13 @@ DEALINGS IN THE SOFTWARE.
 #ifndef CODAL_STM32_I2C_H
 #define CODAL_STM32_I2C_H
 
+#include <vector>
 
 #include "CodalConfig.h"
 #include "ErrorNo.h"
 #include "I2C.h"
 
 #include "STM32Pin.h"
-//#include "i2c.h"
 #include "pinmap.h"
 
 #include "twi.h"
@@ -38,8 +38,11 @@ namespace codal {
 
 class STM32I2C : public codal::I2C {
   private:
-    i2c_t _i2c;
-    //I2C_HandleTypeDef i2c;
+    i2c_t i2c;
+
+    uint16_t currentAddress;
+    std::vector<uint8_t> dataToSent;
+    bool isOnTransmission;
 
   public:
     STM32I2C(STM32Pin& sda, STM32Pin& scl);
@@ -53,11 +56,68 @@ class STM32I2C : public codal::I2C {
     /** Get the I2C buffer size
      * 
      */
-    unsigned getBufferSize(){ return 1000; } // 1 kByte. p. 83
+    unsigned getBufferSize(){ return I2C_TXRX_BUFFER_SIZE; }
 
+    /**
+     * @brief Start I2C transmission with a slave.
+     * 
+     * @param address The slave address
+     */
+    void beginTransmission( uint16_t address );
 
-    void begin();
-    void end();
+    /**
+     * @brief Terminate the I2C transmission. Sending all data to the slave.
+     * 
+     * @param sendStop If True sending the STOP signal. (default: True)
+     */
+    void endTransmission(bool sendStop = true);
+
+    /**
+     * @brief Write single byte to TX buffer.
+     * 
+     * @param data byte to write to buffer
+     * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed. 
+     */
+    int write( uint8_t data ) final override;
+
+    /**
+     * @brief Write bytes to TX buffer
+     * 
+     * @param data pointer to the bytes to write
+     * @param len the number of bytes to write
+     * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed. 
+     */
+    int write( uint8_t *data, size_t len );
+
+    /**
+     * @brief Write byte to the slave's register
+     * 
+     * @param reg register address
+     * @param value byte to send
+     */
+    void writeRegister( uint8_t reg, uint8_t value );
+
+    /**
+     * @brief Read data from slave
+     * 
+     * @param address the slave addres
+     * @param len amount data to read
+     * @param sendStop If True sending the STOP signal. (default: True)
+     * @return bytes received from slave
+     */
+    std::vector<uint8_t> read( uint8_t address, size_t len, bool sendStop = true );
+
+    /**
+     * @brief Read value from slave's register
+     * 
+     * @param address the slave address
+     * @param reg the register address
+     * @param len amount of data to read
+     * @param sendStop If True sending the STOP signal. (default: True)
+     * @return bytes received from slave
+     */
+    std::vector<uint8_t> readRegister( uint8_t address, uint8_t reg, size_t len, bool sendStop = true );
+
 
   protected:
     /**
@@ -73,16 +133,6 @@ class STM32I2C : public codal::I2C {
     int stop() final override { return DEVICE_NOT_IMPLEMENTED; }
 
     /**
-     * Writes the given byte to the I2C bus.
-     *
-     * The CPU will busy wait until the transmission is complete.
-     *
-     * @param data The byte to write.
-     * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
-     */
-    int write(uint8_t data) final override { return DEVICE_NOT_IMPLEMENTED; }
-
-    /**
      * Reads a single byte from the I2C bus.
      * The CPU will busy wait until the transmission is complete.
      *
@@ -90,16 +140,6 @@ class STM32I2C : public codal::I2C {
      */
     int read(AcknowledgeType ack = ACK) final override { return DEVICE_NOT_IMPLEMENTED; }
 
-    /**
-     * @brief Setup pin
-     * 
-     * @param pin 
-     * @param map 
-     * @return void* 
-     */
-    void * setup_pin(PinName pin, const PinMap *map);
-
-  public:
     /**
      * Issues a standard, I2C command write to the I2C bus.
      * This consists of:
@@ -117,7 +157,7 @@ class STM32I2C : public codal::I2C {
      *
      * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
      */
-    int write(uint16_t address, uint8_t* data, int len, bool repeated = false) final override;
+    int write(uint16_t address, uint8_t* data, int len, bool repeated = false) final override { return DEVICE_NOT_IMPLEMENTED; }
 
     /**
      * Performs a typical register write operation to the I2C slave device provided.
@@ -136,7 +176,7 @@ class STM32I2C : public codal::I2C {
      *
      * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
      */
-    int writeRegister(uint16_t address, uint8_t reg, uint8_t value) final override;
+    int writeRegister(uint16_t address, uint8_t reg, uint8_t value) final override { return DEVICE_NOT_IMPLEMENTED; }
 
     /**
      * Issues a standard, 2 byte I2C command read to the I2C bus.
@@ -155,7 +195,7 @@ class STM32I2C : public codal::I2C {
      *
      * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the read request failed.
      */
-    int read(uint16_t address, uint8_t* data, int len, bool repeated = false) final override;
+    int read(uint16_t address, uint8_t* data, int len, bool repeated = false) final override { return DEVICE_NOT_IMPLEMENTED; }
 
     /**
      * Performs a typical register read operation to the I2C slave device provided.
@@ -180,7 +220,7 @@ class STM32I2C : public codal::I2C {
      *
      * @return DEVICE_OK or DEVICE_I2C_ERROR if the the read request failed.
      */
-    int readRegister(uint16_t address, uint8_t reg, uint8_t* data, int length, bool repeated = true) final override;
+    int readRegister(uint16_t address, uint8_t reg, uint8_t* data, int length, bool repeated = true) final override { return DEVICE_NOT_IMPLEMENTED; }
 };
 }  // namespace codal
 
--- src/STM32I2C.cpp
+++ src/STM32I2C.cpp
@@ -1,94 +1,105 @@
+#include <algorithm>
+
 #include "STM32I2C.h"
 #include "PeripheralPins.h"
 
+using namespace std;
 using namespace codal;
 
-STM32I2C::STM32I2C(STM32Pin& sda, STM32Pin& scl) : I2C(sda, scl) {
+STM32I2C::STM32I2C(STM32Pin& sda, STM32Pin& scl) : I2C(sda, scl), currentAddress(0), isOnTransmission(false) {
 
-    //if( sda.name != PinNumber::I2C2_SDA ){ return; }
-
-    /*IC2x_Init_Handler(&i2c);
-
-    void * inst = 0;
-    inst = codal_setup_pin((PinName)sda.name, inst, PinMap_I2C_SDA);
-    inst = codal_setup_pin((PinName)scl.name, inst, PinMap_I2C_SCL);
-    i2c.Instance = (I2C_TypeDef *)inst;
-
-    i2c.Init.Timing = 100000;
-
-    I2Cx_Init(&i2c);*/
-    
-    _i2c.sda = (PinName)sda.name;
-    _i2c.scl = (PinName)scl.name;
-    _i2c.isMaster = 1;
-    _i2c.generalCall = 0;
-
-    //i2c_init(&_i2c);
-
-    /**Configure Analogue filter */
-    // HAL_I2CEx_ConfigAnalogFilter(&_i2c.handle, I2C_ANALOGFILTER_ENABLE);  
-}
-
-void STM32I2C::begin(){
-    i2c_init(&_i2c);
-    //i2c_custom_init(&_i2c, 100000, I2C_ADDRESSINGMODE_7BIT, 0x33);
-}
-
-void STM32I2C::end(){
-    i2c_deinit(&_i2c);
+    i2c.sda = (PinName)sda.name;
+    i2c.scl = (PinName)scl.name;
+    i2c.isMaster = 1;
+    i2c.generalCall = 0;
 }
 
 int STM32I2C::setFrequency(uint32_t frequency) {
+    i2c_setTiming(&i2c, frequency);
     return DEVICE_OK;
 }
 
-int STM32I2C::write(uint16_t address, uint8_t* data, int len, bool repeated) {
+void STM32I2C::beginTransmission( uint16_t address ){
+    if( isOnTransmission ){ return; }
 
-    // int ret = DEVICE_I2C_ERROR;
+    isOnTransmission = true;
+    currentAddress = address;
+    dataToSent.clear();
+}
 
-    // auto res = HAL_I2C_Master_Transmit(&(_i2c.handle), address, data, len, HAL_MAX_DELAY);
-    // if (res == HAL_OK)
-    //     return DEVICE_OK;
-    
-    // return ret;
+void STM32I2C::endTransmission(bool sendStop){
 
-    // uint32_t XferOptions = _i2c.handle.XferOptions;
-    // HAL_I2C_Master_Seq_Transmit_IT(&(_i2c.handle), address, data, len, XferOptions);
+    if( ! isOnTransmission ){ return; }
 
-    // return len;
+    i2c_init(&i2c);
 
-    _i2c.handle.XferOptions = I2C_OTHER_AND_LAST_FRAME;
-    auto res = i2c_master_write(&_i2c, address, data, len);
+    #if defined(I2C_OTHER_FRAME)
+        if( sendStop ){
+            i2c.handle.XferOptions = I2C_OTHER_AND_LAST_FRAME;
+        }
+        else{
+            i2c.handle.XferOptions = I2C_OTHER_FRAME;
+        }
+    #endif
 
-    if( res != I2C_OK ){
-       return DEVICE_I2C_ERROR;
+    unsigned packets = dataToSent.size() / getBufferSize();
+    if( dataToSent.size() - packets * getBufferSize() > 0 ){
+        packets++;
     }
 
-    return len;
+    for( unsigned i = 0; i < packets; ++i ){
+        auto offset = i * getBufferSize();
+        auto length = min<unsigned>( dataToSent.size() - offset, getBufferSize() );
+
+        i2c_master_write(&i2c, currentAddress, dataToSent.data() + offset, length );
+    }
+
+    i2c_deinit(&i2c);
+    isOnTransmission = false;
+    currentAddress = 0;
 }
 
-int STM32I2C::read(uint16_t address, uint8_t* data, int len, bool repeated) {
-    if (data == NULL || len <= 0)
-        return DEVICE_INVALID_PARAMETER;
+int STM32I2C::write( uint8_t data ){
+    if( ! isOnTransmission ) { return 0;}
 
-    int ret = DEVICE_I2C_ERROR;
+    dataToSent.push_back( data );
 
-    return ret;
+    return DEVICE_OK;
 }
 
-int STM32I2C::readRegister(uint16_t address, uint8_t reg, uint8_t* data, int length, bool repeated) {
-    if (data == NULL || length <= 0)
-        return DEVICE_INVALID_PARAMETER;
-    return 0;
+int STM32I2C::write( uint8_t * data, size_t len ){
+    if( ! isOnTransmission ){ return 0; }
+
+    dataToSent.insert( dataToSent.begin(), data, data + len );
+
+    return DEVICE_OK;
 }
 
-int STM32I2C::writeRegister(uint16_t address, uint8_t reg, uint8_t value) {
-    return 0;
+void STM32I2C::writeRegister( uint8_t reg, uint8_t value){
+    write(reg);
+    write(value);
 }
 
-void * STM32I2C::setup_pin(PinName pin, const PinMap *map)
-{
-    void * tmp = pinmap_peripheral(pin, map);
-    pin_function(pin, pinmap_function(pin, map));
-    return tmp;
+vector<uint8_t> STM32I2C::read( uint8_t address, size_t len, bool sendStop ){
+    vector<uint8_t> data(len);
+
+    #if defined(I2C_OTHER_FRAME)
+        if (sendStop) {
+            i2c.handle.XferOptions = I2C_OTHER_AND_LAST_FRAME;
+        } else {
+            i2c.handle.XferOptions = I2C_OTHER_FRAME ;
+        }
+    #endif
+
+    i2c_master_read( &i2c, address, data.data(), len );
+
+    return data;
+}
+
+vector<uint8_t> STM32I2C::readRegister( uint8_t address, uint8_t reg, size_t len, bool sendStop ){
+    beginTransmission(address);
+        write(reg);
+    endTransmission(false);
+
+    return read(address, len, sendStop);
 }
