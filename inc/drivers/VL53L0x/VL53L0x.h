c77fefe71ff21ffed93ff47a17fde6818089eaa2
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-01-28 09:51:13 +0100

    First version of VL53L0x driver

---
+++ inc/drivers/VL53L0x/VL53L0x.h
@@ -0,0 +1,29 @@
+#ifndef __VL53L0X_H__
+#define __VL53L0X_H__
+
+#include "STM32I2C.h"
+#include "STM32Pin.h"
+
+#include "vl53l0x_class.h"
+
+namespace codal {
+
+    class VL53L0x {
+
+        private:
+            VL53L0X_base baseDriver;
+            uint8_t address;
+
+        public:
+            VL53L0x( STM32I2C& i2c, STM32Pin& pin, uint8_t address );
+
+            void init();
+            void enable();
+            void disable();
+
+            uint32_t getDistance();
+    };
+
+}
+
+#endif
