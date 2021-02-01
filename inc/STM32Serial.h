0070dd176508ea9809dbaa6c982394a36923dc20
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-02-01 18:38:29 +0100

    First version of Serial driver

---
+++ inc/STM32Serial.h
@@ -0,0 +1,66 @@
+#ifndef __STM32SERIAL_H__
+#define __STM32SERIAL_H__
+
+#include <map>
+
+#include "Serial.h"
+#include "uart.h"
+
+#include "STM32Pin.h"
+
+namespace codal{
+
+    enum LengthSerial {
+        SERIAL_7_BITS = UART_WORDLENGTH_7B,
+        SERIAL_8_BITS = UART_WORDLENGTH_8B,
+        SERIAL_9_BITS = UART_WORDLENGTH_9B
+    };
+
+    enum ParitySerial {
+        SERIAL_NONE_PARITY = UART_PARITY_NONE,
+        SERIAL_ODD_PARITY  = UART_PARITY_ODD,
+        SERIAL_EVEN_PARITY = UART_PARITY_EVEN
+    };
+
+    enum StopBitSerial {
+        SERIAL_STOP_0_5 = UART_STOPBITS_0_5,
+        SERIAL_STOP_1   = UART_STOPBITS_1,
+        SERIAL_STOP_1_5 = UART_STOPBITS_1_5,
+        SERIAL_STOP_2   = UART_STOPBITS_2
+    };
+
+    class STM32Serial : public Serial {
+
+        private:
+            static std::map<serial_t*, STM32Serial*> mapSerialInstance;
+
+            STM32Pin& rxPin;
+            STM32Pin& txPin;
+            serial_t serial;
+            uint32_t baudrate;
+            LengthSerial databits;
+            ParitySerial parity;
+            StopBitSerial stopBit;
+
+        protected:
+            int enableInterrupt(SerialInterruptType t) final override;
+            int disableInterrupt(SerialInterruptType t) final override;
+            int setBaudrate(uint32_t baudrate) final override;
+            int configurePins(Pin& tx, Pin& rx) final override;
+            
+            static void RxIRQ( serial_t * obj );
+
+        public:
+            STM32Serial( STM32Pin& rxPin, STM32Pin& txPin );
+            ~STM32Serial();
+
+            void init( uint32_t baudrate, LengthSerial databits = SERIAL_8_BITS, ParitySerial parity = SERIAL_NONE_PARITY, StopBitSerial stopBit = SERIAL_STOP_1 );
+
+            int putc(char c) final override;
+            int getc() final override;
+    };
+    
+    extern STM32Serial* default_serial_debug;
+}
+
+#endif
