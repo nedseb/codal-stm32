8bb101c785cd6d77c7adbc7c1b18720f092024d4
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-02-04 08:27:23 +0200

    Add declaration of `__io_putchar` and `__io_getchar` (#18)

--- src/syscalls.c
+++ src/syscalls.c
@@ -13,6 +13,9 @@
 #undef errno
 extern int errno;
 
+extern int __io_putchar(int ch) __attribute__((weak));
+extern int __io_getchar(void) __attribute__((weak));
+
 extern size_t uart_debug_write(uint8_t *data, uint32_t size);
 
 // Helper macro to mark unused parameters and prevent compiler warnings.
