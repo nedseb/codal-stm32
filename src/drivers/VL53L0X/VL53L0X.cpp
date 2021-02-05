f37d2f5378c6e3e7afbc3ac962be18fc9777583d
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-02-05 14:23:45 +0100

    Rename VL53L0X driver's files

---
+++ src/drivers/VL53L0X/VL53L0X.cpp
@@ -0,0 +1,26 @@
+#include "VL53L0X.h"
+
+using namespace codal;
+
+
+VL53L0X::VL53L0X( STM32I2C& i2c, STM32Pin& shutPin, uint8_t address ) : baseDriver( i2c, shutPin, address ), address(address) {
+
+}
+
+void VL53L0X::init(){
+    baseDriver.InitSensor(address);
+}
+
+void VL53L0X::enable(){
+    baseDriver.VL53L0X_On();
+}
+
+void VL53L0X::disable(){
+    baseDriver.VL53L0X_Off();
+}
+
+uint32_t VL53L0X::getDistance(){
+    uint32_t distance;
+    baseDriver.GetDistance(&distance);
+    return distance;
+}
