3fbe9825acdd20fada2530b62afb583eb9b69b90
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-02-05 14:00:20 +0100

    Add LPS22HB compliance Codal class

---
+++ inc/drivers/LPS22HB/LPS22HB_Pressure.h
@@ -0,0 +1,33 @@
+#ifndef __LPS22HB_PRESSURE_H__
+#define __LPS22HB_PRESSURE_H__
+
+
+#include "LPS22HB.h"
+#include "Sensor.h"
+
+namespace codal{
+
+class LPS22HB_Pressure : public LPS22HB, public Sensor{
+    LPS22HB_Pressure( uint8_t id, STM32I2C& i2c, uint8_t address = 0xB8, uint16_t sensitivity = SENSOR_DEFAULT_SENSITIVITY, uint16_t samplePeriod = SENSOR_DEFAULT_SAMPLE_PERIOD );
+
+    /**
+     * @brief Configure the sensor
+     * 
+     */
+    void configure();
+
+    /**
+     * Read the value from underlying hardware.
+     */
+    virtual int readValue( ){
+        if(!isInitialized){ configure(); }
+        return getPressure();
+    };
+
+    private:
+        bool isInitialized;
+};
+
+}
+
+#endif
