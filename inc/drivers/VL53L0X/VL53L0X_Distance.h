6230dd1e920d6fe515c38fd0405b4a814024b372
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-02-05 14:24:25 +0100

    Add VL53L0X compliance Codal class

---
+++ inc/drivers/VL53L0X/VL53L0X_Distance.h
@@ -0,0 +1,32 @@
+#ifndef __VL53L0X_DISTANCE_H__
+#define __VL53L0X_DISTANCE_H__
+
+#include "VL53L0X.h"
+#include "Sensor.h"
+
+namespace codal{
+
+class VL53L0X_Distance : public VL53L0X, public Sensor {
+    VL53L0X_Distance( uint8_t id, STM32I2C& i2c, STM32Pin& shutPin, uint8_t address, uint16_t sensitivity = SENSOR_DEFAULT_SENSITIVITY, uint16_t samplePeriod = SENSOR_DEFAULT_SAMPLE_PERIOD );
+
+    /**
+     * @brief Configure the sensor
+     * 
+     */
+    void configure();
+
+    /**
+     * Read the value from underlying hardware.
+     */
+    virtual int readValue( ){
+        if(!isInitialized){ configure(); }
+        return getDistance();
+    };
+
+    private:
+        bool isInitialized;
+};
+
+}
+
+#endif
