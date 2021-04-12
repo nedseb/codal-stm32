9d1046213deba0d2af248310b0ba20907365985e
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-04-12 12:19:51 +0000

    Add 'ifdef USBCON' protection

--- inc/STM32USBSerial.h
+++ inc/STM32USBSerial.h
@@ -1,35 +1,39 @@
 #ifndef __STM32USBSERIAL_H__
 #define __STM32USBSERIAL_H__
 
+#ifdef USBCON
+
 #include <cstdint>
-#include "Serial.h"
+
 #include "STM32Pin.h"
+#include "Serial.h"
 
 namespace codal {
-    class STM32USBSerial : public Serial {
+class STM32USBSerial : public Serial {
+  public:
+    STM32USBSerial();
+    ~STM32USBSerial() {}
 
-        public:
-            STM32USBSerial();
-            ~STM32USBSerial() {}
+    int init() final override;
+    int putc(char c) final override;
+    int getc() final override { return this->getChar(ASYNC); }
 
-            int init() final override;
-            int putc(char c) final override;
-            int getc() final override { return this->getChar(ASYNC); }
+    bool isConnected();
 
-            bool isConnected();
+  protected:
+    int enableInterrupt(SerialInterruptType t);
+    int disableInterrupt(SerialInterruptType t) final override { return DEVICE_OK; }
+    int setBaudrate(uint32_t baudrate) final override { return DEVICE_OK; }
+    int configurePins(Pin& tx, Pin& rx) final override { return DEVICE_OK; }
 
-        protected:
-            int enableInterrupt(SerialInterruptType t);
-            int disableInterrupt(SerialInterruptType t) final override { return DEVICE_OK; }
-            int setBaudrate(uint32_t baudrate) final override { return DEVICE_OK; }
-            int configurePins(Pin& tx, Pin& rx) final override { return DEVICE_OK; }
+  private:
+    STM32Pin nonePin;
+    static STM32USBSerial* instance;
 
-        private:
-            STM32Pin nonePin;
-            static STM32USBSerial* instance;
-            
-            static void receivedCallback();
-    };
-}
+    static void receivedCallback();
+};
+}  // namespace codal
+
+#endif /* USBCON */
 
 #endif
