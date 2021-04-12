dcb4a5ebe045d1ec2e4d751bee35a248e1b61ba7
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-04-12 09:11:36 +0000

    Attach class to CodalComponent

--- inc/STM32WebUSB.h
+++ inc/STM32WebUSB.h
@@ -1,16 +1,17 @@
 #ifndef __STM32WEBUSB_H__
 #define __STM32WEBUSB_H__
 
+#include "CodalComponent.h"
+
 namespace codal {
-    class STM32WebUSB {
+class STM32WebUSB : public CodalComponent {
+  public:
+    STM32WebUSB();
+    ~STM32WebUSB() {}
 
-        public:
-            STM32WebUSB();
-            ~STM32WebUSB() {}
-
-            void init();
-            void deInit();
-    };
-}
+    int init();
+    void deInit();
+};
+}  // namespace codal
 
 #endif /* __STM32WEBUSB_H__ */
