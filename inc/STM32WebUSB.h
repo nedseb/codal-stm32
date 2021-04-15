01edfe3d4fb0245a05f47844732bd1133c9361f4
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-04-15 11:50:09 +0000

    Adapt WebUSB class to support CDC

--- inc/STM32WebUSB.h
+++ inc/STM32WebUSB.h
@@ -1,21 +1,39 @@
-#ifndef __STM32WEBUSB_H__
-#define __STM32WEBUSB_H__
+#ifndef __STM32USBSERIAL_H__
+#define __STM32USBSERIAL_H__
 
 #ifdef USBCON
 
-#include "CodalComponent.h"
+#include <cstdint>
+
+#include "STM32Pin.h"
+#include "Serial.h"
 
 namespace codal {
-class STM32WebUSB : public CodalComponent {
+class STM32WebUSB : public Serial {
   public:
     STM32WebUSB();
     ~STM32WebUSB() {}
 
-    int init();
-    void deInit();
+    int init() final override;
+    int putc(char c) final override;
+    int getc() final override { return this->getChar(ASYNC); }
+
+    bool isConnected();
+
+  protected:
+    int enableInterrupt(SerialInterruptType t);
+    int disableInterrupt(SerialInterruptType t) final override { return DEVICE_OK; }
+    int setBaudrate(uint32_t baudrate) final override { return DEVICE_OK; }
+    int configurePins(Pin& tx, Pin& rx) final override { return DEVICE_OK; }
+
+  private:
+    STM32Pin nonePin;
+    static STM32WebUSB* instance;
+
+    static void receivedCallback();
 };
 }  // namespace codal
 
 #endif /* USBCON */
 
-#endif /* __STM32WEBUSB_H__ */
+#endif
--- src/STM32WebUSB.cpp
+++ src/STM32WebUSB.cpp
@@ -1,23 +1,75 @@
+
 #ifdef USBCON
 
 #include "STM32WebUSB.h"
 
+#include "PinNumber.h"
 #include "usbd_webusb_if.h"
 
 using namespace codal;
 
-STM32WebUSB::STM32WebUSB() : CodalComponent(DEVICE_ID_SERIAL, 0) {}
+STM32WebUSB* STM32WebUSB::instance = nullptr;
+
+STM32WebUSB::STM32WebUSB() : Serial(nonePin, nonePin), nonePin(0, PinNumber::NC, PIN_CAPABILITY_ALL)
+{
+    if (instance == nullptr) instance = this;
+}
 
 int STM32WebUSB::init()
 {
     webUSB_init();
+    webUSB_OnDataReceived(receivedCallback);
 
     return DEVICE_OK;
 }
 
-void STM32WebUSB::deInit()
+int STM32WebUSB::putc(char c)
 {
-    webUSB_deInit();
+    uint8_t value = c;
+
+    CDC_TransmitQueue_Enqueue(&WebUSB_TransmitQueue, &value, 1);
+
+    if (webUSB_connected()) {
+        webUSB_continue_transmit();
+    }
+
+    return DEVICE_OK;
+}
+
+void STM32WebUSB::receivedCallback()
+{
+    int length = CDC_ReceiveQueue_ReadSize(&WebUSB_ReceiveQueue);
+
+    for (int i = 0; i < length; ++i) {
+        int byte = CDC_ReceiveQueue_Dequeue(&WebUSB_ReceiveQueue);
+        webUSB_resume_receive();
+
+        uint16_t pos = (unsigned int)(instance->rxBuffHead + 1) % instance->rxBuffSize;
+
+        if (pos != instance->rxBuffTail) {
+            instance->rxBuff[instance->rxBuffHead] = (uint8_t)byte;
+            instance->rxBuffHead                   = pos;
+        }
+    }
+}
+
+int STM32WebUSB::enableInterrupt(SerialInterruptType t)
+{
+    if (t == RxInterrupt) {
+        return DEVICE_OK;
+    }
+    else if (t == TxInterrupt) {
+        while (txBufferedSize() > 0) {
+            dataTransmitted();
+        }
+    }
+
+    return DEVICE_OK;
+}
+
+bool STM32WebUSB::isConnected()
+{
+    return webUSB_connected();
 }
 
 #endif /* USBCON */
