d241bbbdada8d1fba7718c6ab5d6dab9d13eb001
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-06-08 14:03:22 +0000

    Import abstraction layer from STM32Duino

---
+++ source/board.h
@@ -0,0 +1,14 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include "stm32_def.h"
+#include "hw_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+void init(void);
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* _BOARD_H_ */
