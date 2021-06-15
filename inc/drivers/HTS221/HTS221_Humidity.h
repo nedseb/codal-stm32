5887f9b7db9b41a64fbe3c37ae9d484c5aa28c98
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-06-15 14:21:38 +0000

    Import HTS221 Driver

---
+++ inc/drivers/HTS221/HTS221_Humidity.h
@@ -0,0 +1,34 @@
+#ifndef __SSH221_HUMIDITY_H__
+#define __SSH221_HUMIDITY_H__
+
+#include "HTS221.h"
+#include "STM32I2C.h"
+#include "Sensor.h"
+
+namespace codal{
+
+class HTS221_Humidity : public HTS221, public Sensor {
+    public:
+        HTS221_Humidity( uint8_t id, STM32I2C& i2c, uint16_t address, uint16_t sensitivity = SENSOR_DEFAULT_SENSITIVITY, uint16_t samplePeriod = SENSOR_DEFAULT_SAMPLE_PERIOD );
+
+        /**
+         * @brief Configure the sensor
+         * 
+         */
+        void configure();
+
+        /**
+         * Read the value from underlying hardware.
+         */
+        virtual int readValue( ) override final{
+            if(!isInitialized) configure();
+            return (int)getHumidity();
+        }
+
+    private:
+        bool isInitialized;
+};
+
+}
+
+#endif
---
+++ source/drivers/HTS221/HTS221_Humidity.cpp
@@ -0,0 +1,16 @@
+#include "HTS221_Humidity.h"
+
+using namespace codal;
+
+HTS221_Humidity::HTS221_Humidity( uint8_t id, STM32I2C& i2c, uint16_t address, uint16_t sensitivity, uint16_t samplePeriod )
+    : HTS221( i2c, address ), Sensor( id, sensitivity, samplePeriod ), isInitialized(false) {
+    configure();
+}
+
+void HTS221_Humidity::configure(){
+    HTS221::init();
+    setOutputRate( HTS221_OUTPUT_RATE::RATE_7HZ );
+
+    isInitialized = true;
+}
+
