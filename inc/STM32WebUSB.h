58fc325b46ed1ddb3789a6cbb3ad4142d2376e96
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-06-17 15:12:33 +0000

    Import WebUSBSerial and USBSerial

---
+++ inc/STM32WebUSB.h
@@ -0,0 +1,43 @@
+#ifndef __STM32USBSERIAL_H__
+#define __STM32USBSERIAL_H__
+
+#ifdef USBCON
+
+#include <cstdint>
+
+#include "STM32Pin.h"
+#include "Serial.h"
+
+// required for gcc-6 (but not 7!)
+#undef putc
+#undef getc
+
+namespace codal {
+class STM32WebUSB : public Serial {
+  public:
+    STM32WebUSB();
+    ~STM32WebUSB() {}
+
+    int init() final override;
+    int putc(char c) final override;
+    int getc() final override { return this->getChar(ASYNC); }
+
+    bool isConnected();
+
+  protected:
+    int enableInterrupt(SerialInterruptType t);
+    int disableInterrupt(SerialInterruptType t) final override { return DEVICE_OK; }
+    int setBaudrate(uint32_t baudrate) final override { return DEVICE_OK; }
+    int configurePins(Pin& tx, Pin& rx) final override { return DEVICE_OK; }
+
+  private:
+    STM32Pin nonePin;
+    static STM32WebUSB* instance;
+
+    static void receivedCallback();
+};
+}  // namespace codal
+
+#endif /* USBCON */
+
+#endif
