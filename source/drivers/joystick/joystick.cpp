156b6d654de578d26f00b82cd2f39b57c0d96cdf
Pascal-Flores <pascal.flores@etu.univ-amu.fr>
2022-04-26 17:11:52 +0000

    deadzone and threshold management added

--- source/drivers/joystick/joystick.cpp
+++ source/drivers/joystick/joystick.cpp
@@ -13,3 +13,26 @@ Joystick::Joystick(codal::STM32Pin& horizontalAxisPin, codal::STM32Pin& vertical
     verticalSensor   = new AnalogSensor(verticalAxisPin, 9999);
     button           = new Button(buttonPin, 7777);
 }
+
+void Joystick::setDeadzone(uint8_t newDeadzone)
+{
+    if (newDeadzone > 100) {
+        newDeadzone = 100;
+    }
+    this->deadzone = round((float)newDeadzone / 100 * 512);
+    setThresholds();
+    return;
+}
+
+uint8_t Joystick::getDeadzone()
+{
+    return round((float)this->deadzone / 512 * 100);
+}
+
+void Joystick::setThresholds()
+{
+    this->horizontalSensor->setHighThreshold(512 + deadzone);
+    this->horizontalSensor->setLowThreshold(512 - deadzone);
+    this->verticalSensor->setHighThreshold(512 + deadzone);
+    this->verticalSensor->setLowThreshold(512 - deadzone);
+}
