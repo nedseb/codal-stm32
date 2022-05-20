62d30f44687e10be1411fb0a4d0921fe5ed82084
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-05-20 20:35:17 +0000

    Add pressure sensor driver

---
+++ inc/drivers/WSEN-PADS/WSEN-PADS.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <vector>
+
+#include "STM32I2C.h"
+
+namespace codal {
+
+enum class WSEN_PADS_ODR : uint8_t {
+    SINGLE      = 0b000,
+    RATE_1_HZ   = 0b001,
+    RATE_10_HZ  = 0b010,
+    RATE_25_HZ  = 0b011,
+    RATE_50_HZ  = 0b100,
+    RATE_75_HZ  = 0b101,
+    RATE_100_HZ = 0b110,
+    RATE_200_HZ = 0b111,
+};
+
+class WSEN_PADS {
+  public:
+    /**
+     * @brief Construct a new wsen pads object
+     *
+     * @param i2c
+     * @param address
+     */
+    WSEN_PADS(STM32I2C& i2c, uint16_t address = 0xB8) : i2c(i2c), address(address){};
+
+    ~WSEN_PADS() {}
+
+    /**
+     * @brief Initialise the sensor
+     *
+     */
+    void init();
+
+    uint8_t whoAmI();
+
+    /**
+     * @brief Change the Output Data Rate
+     *
+     * @param odr The rate to apply (default 10 Hz)
+     */
+    void setODR(WSEN_PADS_ODR odr)
+    {
+        this->odr = odr;
+        on();
+    }
+
+    /**
+     * @brief Set the Low Noise Mode
+     *
+     * @param enable if TRUE the low Noise mode will be enable (default), otherwise the low power mode will be enable
+     */
+    void setLowNoiseMode(bool enable);
+
+    /**
+     * @brief Set the second Low Pass Filter
+     *
+     * @param enable if TRUE the second lowpass filter will be enable, otherwise FALSE will disable it.
+     */
+    void setLowPassFilter(bool enable);
+
+    /**
+     * @brief Set the Block data update feature. The BDU blocks the data register to be updated until the register are
+     * readed.
+     *
+     * @param enable if TRUE the BDU will be enable, otherwise FALSE will disable it.
+     */
+    void setBDU(bool enable);
+
+    /**
+     * @brief Stop measuring and enter in power-down mode
+     *
+     */
+    void off();
+
+    /**
+     * @brief Start measuring (only if ODR not equal to SINGLE)
+     *
+     */
+    void on();
+
+    /**
+     * @brief When the ODR is equal to SINGLE, use this fonction to start a measure.
+     *
+     */
+    void oneShotMeasure();
+
+    /**
+     * @brief Get the Pressure
+     *
+     * @return float the pressure value in kilo Pascal (kPa)
+     */
+    float getPressure();
+
+    /**
+     * @brief Get the Temperature
+     *
+     * @return float the temperature value in Celsius (C)
+     */
+    float getTemperature();
+
+  private:
+    STM32I2C& i2c;
+    WSEN_PADS_ODR odr;
+    uint16_t address;
+
+    void setRegister(uint8_t regAddr, uint8_t value);
+    uint8_t getRegister(uint8_t regAddr) { return i2c.readRegister(address, regAddr, 1)[0]; }
+    void setBitRegister(uint8_t regAddr, uint8_t bit);
+    void clearBitRegister(uint8_t regAddr, uint8_t bit);
+};
+}  // namespace codal
---
+++ source/drivers/WSEN-PADS/WSEN-PADS.cpp
@@ -0,0 +1,138 @@
+#include "WSEN-PADS.h"
+
+using namespace codal;
+
+constexpr uint8_t INT_CFG        = 0x0B;
+constexpr uint8_t THR_P_L        = 0x0C;
+constexpr uint8_t THR_P_H        = 0x0D;
+constexpr uint8_t INTERFACE_CTRL = 0x0E;
+constexpr uint8_t DEVICE_ID      = 0x0F;
+constexpr uint8_t CTRL_1         = 0x10;
+constexpr uint8_t CTRL_2         = 0x11;
+constexpr uint8_t CTRL_3         = 0x12;
+constexpr uint8_t FIFO_CTRL      = 0x13;
+constexpr uint8_t FIFO_WTM       = 0x14;
+constexpr uint8_t REF_P_L        = 0x15;
+constexpr uint8_t REF_P_H        = 0x16;
+constexpr uint8_t OPC_P_L        = 0x18;
+constexpr uint8_t OPC_P_H        = 0x19;
+constexpr uint8_t INT_SOURCE     = 0x24;
+constexpr uint8_t FIFO_STATUS_1  = 0x25;
+constexpr uint8_t FIFO_STATUS_2  = 0x26;
+constexpr uint8_t STATUS         = 0x27;
+constexpr uint8_t DATA_P_XL      = 0x28;
+constexpr uint8_t DATA_P_L       = 0x29;
+constexpr uint8_t DATA_P_H       = 0x2A;
+constexpr uint8_t DATA_T_L       = 0x2B;
+constexpr uint8_t DATA_T_H       = 0x2C;
+constexpr uint8_t FIFO_DATA_P_XL = 0x78;
+constexpr uint8_t FIFO_DATA_P_L  = 0x79;
+constexpr uint8_t FIFO_DATA_P_H  = 0x7A;
+constexpr uint8_t FIFO_DATA_T_L  = 0x7B;
+constexpr uint8_t FIFO_DATA_T_H  = 0x7C;
+
+void WSEN_PADS::init()
+{
+    off();
+    setLowNoiseMode(true);
+    setLowPassFilter(true);
+    setBDU(true);
+
+    setODR(WSEN_PADS_ODR::RATE_10_HZ);
+}
+
+uint8_t WSEN_PADS::whoAmI()
+{
+    return getRegister(DEVICE_ID);
+}
+
+void WSEN_PADS::setLowNoiseMode(bool enable)
+{
+    if (enable)
+        setBitRegister(CTRL_2, 1);
+    else
+        clearBitRegister(CTRL_2, 1);
+}
+
+void WSEN_PADS::setLowPassFilter(bool enable)
+{
+    if (enable)
+        setBitRegister(CTRL_1, 2);
+    else
+        clearBitRegister(CTRL_1, 2);
+}
+
+void WSEN_PADS::setBDU(bool enable)
+{
+    if (enable)
+        setBitRegister(CTRL_1, 1);
+    else
+        clearBitRegister(CTRL_1, 1);
+}
+
+void WSEN_PADS::off()
+{
+    auto reg = getRegister(CTRL_1);
+    reg &= ~(0b111 << 4);
+    setRegister(CTRL_1, reg);
+}
+
+void WSEN_PADS::on()
+{
+    auto reg = getRegister(CTRL_1);
+    reg &= ~(0b111 << 4);
+    reg |= (uint8_t)odr << 4;
+    setRegister(CTRL_1, reg);
+}
+
+void WSEN_PADS::oneShotMeasure()
+{
+    setBitRegister(CTRL_2, 0);
+}
+
+float WSEN_PADS::getPressure()
+{
+    uint8_t xl     = getRegister(DATA_P_XL);
+    uint8_t l      = getRegister(DATA_P_L);
+    uint8_t h      = getRegister(DATA_P_H);
+    int32_t result = (int32_t)xl;
+    result |= (int32_t)(l << 8);
+    result |= (int32_t)(h << 16);
+
+    return (float)result / 40960.0f;
+}
+
+float WSEN_PADS::getTemperature()
+{
+    uint8_t l      = getRegister(DATA_T_L);
+    uint8_t h      = getRegister(DATA_T_H);
+    int16_t result = (int16_t)l;
+    result |= (int16_t)(h << 8);
+
+    return (float)result / 100.0f;
+}
+
+void WSEN_PADS::setRegister(uint8_t regAddr, uint8_t value)
+{
+    i2c.beginTransmission(address);
+    i2c.writeRegister(regAddr, value);
+    i2c.endTransmission();
+}
+
+void WSEN_PADS::setBitRegister(uint8_t regAddr, uint8_t bit)
+{
+    if (bit > 15) return;
+
+    uint8_t reg = getRegister(regAddr);
+    reg &= 1 << bit;
+    setRegister(regAddr, reg);
+}
+
+void WSEN_PADS::clearBitRegister(uint8_t regAddr, uint8_t bit)
+{
+    if (bit > 15) return;
+
+    uint8_t reg = getRegister(regAddr);
+    reg &= ~(1 << bit);
+    setRegister(regAddr, reg);
+}
