c2b4fbb58a88065d7360d18ec780dc4d99be45d6
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-05-23 09:58:13 +0000

    Start improve framebuffer

--- inc/frameBuffer.h
+++ inc/frameBuffer.h
@@ -5,48 +5,47 @@
 #include <string>
 #include <vector>
 
-namespace codal{
+namespace codal {
 
-    class FrameBuffer {
-
-        public:
-            enum Format{
-                MONO_VLSB,
-                // MONO_HLSB,
-                // MONO_HMSB,
-                // RGB565,
-                GS2_HMSB,
-                // GS4_HMSB,
-                // GS8
-            };
-
-            FrameBuffer( unsigned widthPixel, unsigned heightPixel, Format format);
-            ~FrameBuffer();
-
-            void fill(uint16_t value);
-            void drawPixel( unsigned x, unsigned y, uint16_t color);
-            void drawChar(char c, unsigned x, unsigned y, uint16_t color);
-            void drawText(std::string str, unsigned x, unsigned y, uint16_t color);
-            
-            uint16_t getPixelColor( uint8_t x, uint8_t y);
-            void drawSegment(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint8_t size, uint16_t color);
-            void drawLine(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint8_t size, uint16_t color);
-            void drawBezierCurve(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, uint16_t color);
-            void drawRectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, bool fill, uint16_t color);
-            void drawSquare(uint16_t x, uint16_t y, uint16_t lenght, bool fill, uint16_t color);
-            void drawEllipse(int width, int height, int xCenter, int yCenter, bool fill, uint16_t color);
-            void drawCircle(int x, int y, int radius, bool fill, uint16_t color);
-            void drawPolygon(uint8_t x, uint8_t y, uint8_t line, uint8_t radius, uint8_t size, uint16_t color);
-            void drawMatrix( std::vector<std::vector<unsigned>> matrix, unsigned x, unsigned y);
-
-        protected:
-            unsigned width;
-            unsigned height;
-            unsigned bufferSize;
-            Format format;
-            uint8_t pages;
-            uint8_t * buffer;
+class FrameBuffer {
+  public:
+    enum Format {
+        MONO_VLSB,
+        // MONO_HLSB,
+        // MONO_HMSB,
+        // RGB565,
+        // GS2_HMSB,
+        GS4_HMSB,
+        // GS8
     };
-}
+
+    FrameBuffer(unsigned widthPixel, unsigned heightPixel, Format format);
+    ~FrameBuffer();
+
+    void fill(uint16_t value);
+    void drawPixel(unsigned x, unsigned y, uint16_t color);
+    void drawChar(char c, unsigned x, unsigned y, uint16_t color);
+    void drawText(std::string str, unsigned x, unsigned y, uint16_t color);
+
+    uint16_t getPixelColor(uint8_t x, uint8_t y);
+    void drawSegment(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint8_t size, uint16_t color);
+    void drawLine(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint8_t size, uint16_t color);
+    void drawBezierCurve(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t x3, uint8_t y3, uint16_t color);
+    void drawRectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, bool fill, uint16_t color);
+    void drawSquare(uint16_t x, uint16_t y, uint16_t lenght, bool fill, uint16_t color);
+    void drawEllipse(int width, int height, int xCenter, int yCenter, bool fill, uint16_t color);
+    void drawCircle(int x, int y, int radius, bool fill, uint16_t color);
+    void drawPolygon(uint8_t x, uint8_t y, uint8_t line, uint8_t radius, uint8_t size, uint16_t color);
+    void drawMatrix(std::vector<std::vector<unsigned>> matrix, unsigned x, unsigned y);
+
+  protected:
+    unsigned width;
+    unsigned height;
+    unsigned bufferSize;
+    Format format;
+    uint8_t pages;
+    uint8_t* buffer;
+};
+}  // namespace codal
 
 #endif
