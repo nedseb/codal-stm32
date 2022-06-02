cfba6da89eb5e854e29aab73bce6420777f670b6
Adrien Virieux <adrien.virieux@etu.univ-amu.fr>
2022-06-02 15:00:45 +0200

    onEvent set on private

--- inc/drivers/HCSR04/HCSR04Sensor.h
+++ inc/drivers/HCSR04/HCSR04Sensor.h
@@ -1,27 +1,3 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2017 Lancaster University.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-*/
-
 #pragma once
 
 #include "HCSR04.h"
@@ -35,43 +11,21 @@ DEALINGS IN THE SOFTWARE.
 namespace codal
 {
     typedef void (*HCSR04UserEvent)();
-
     enum class DistanceBehold : uint8_t {Near = 0, Far = 1};
 
-    /**
-     * Class definition for a generic analog sensor, and performs periodic sampling, buffering and low pass filtering of the data.
-     */
     class HCSR04Sensor : public HCSR04, public Sensor
     {
-        private:
-        HCSR04UserEvent onNearDistance;
-        HCSR04UserEvent onFarDistance;
-
-
         public:
+            HCSR04Sensor(STM32Pin& trig, STM32Pin& echo, uint16_t id);
 
-        /**
-          * Constructor.
-          *
-          * Creates a generic HCSR04Sensor.
-          *
-          * @param id The ID of this compoenent e.g. DEVICE_ID_THERMOMETER
-         */
-        HCSR04Sensor(STM32Pin& trig, STM32Pin& echo, uint16_t id);
+            void registerDistanceEvent(DistanceBehold oui, uint16_t distance, codal::HCSR04Unit type, HCSR04UserEvent handler);
+          
+            virtual int readValue() override final;
 
-        void registerDistanceEvent(DistanceBehold oui, uint16_t distance, codal::HCSR04Unit type, HCSR04UserEvent handler);
+        private:
+            HCSR04UserEvent onNearDistance;
+            HCSR04UserEvent onFarDistance;
 
-        void onEvent(Event event);
-        
-
-        /**
-         * Read the value from pin.
-         */
-        virtual int readValue() override final;
-
-        /**
-          * Destructor.
-          */
-        // ~HCSR04Sensor();
+            void onEvent(Event event);
     };
 }
