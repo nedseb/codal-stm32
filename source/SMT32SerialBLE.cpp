c0e8271645369c09b6345a3d404d275d876fcec8
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-07-06 08:45:32 +0000

    Comment BLE classes

--- source/SMT32SerialBLE.cpp
+++ source/SMT32SerialBLE.cpp
@@ -1,105 +1,105 @@
-#include <algorithm>
-#include <functional>
+// #include <algorithm>
+// #include <functional>
 
-#include "STM32SerialBLE.h"
+// #include "STM32SerialBLE.h"
 
-using namespace std;
-using namespace codal;
+// using namespace std;
+// using namespace codal;
 
-#define DEVICE_INITIALIZED 0x01
+// #define DEVICE_INITIALIZED 0x01
 
-#define BLE_TX_BUFFER_SIZE 512
-#define BLE_RX_BUFFER_SIZE 512
+// #define BLE_TX_BUFFER_SIZE 512
+// #define BLE_RX_BUFFER_SIZE 512
 
-STM32SerialBLE::STM32SerialBLE(const char* serviceUUID, const char* rxUUID, const char* txUUID)
-    : CodalComponent(STM32_ID_BLE_SERIAL, 0),
-      serialService(serviceUUID),
-      rxSerialCharac(rxUUID, BLEWrite | BLEWriteWithoutResponse, BLE_RX_BUFFER_SIZE),
-      txSerialCharac(txUUID, BLEIndicate, BLE_TX_BUFFER_SIZE),
-      charOnDelimiter(0)
-{
-}
+// STM32SerialBLE::STM32SerialBLE(const char* serviceUUID, const char* rxUUID, const char* txUUID)
+//     : CodalComponent(STM32_ID_BLE_SERIAL, 0),
+//       serialService(serviceUUID),
+//       rxSerialCharac(rxUUID, BLEWrite | BLEWriteWithoutResponse, BLE_RX_BUFFER_SIZE),
+//       txSerialCharac(txUUID, BLEIndicate, BLE_TX_BUFFER_SIZE),
+//       charOnDelimiter(0)
+// {
+// }
 
-void STM32SerialBLE::periodicCallback()
-{
-    if ((status & DEVICE_INITIALIZED) != 0) {
-        BLE.poll();
-    }
-}
+// void STM32SerialBLE::periodicCallback()
+// {
+//     if ((status & DEVICE_INITIALIZED) != 0) {
+//         BLE.poll();
+//     }
+// }
 
-int STM32SerialBLE::begin()
-{
-    if ((status & DEVICE_INITIALIZED) != 0) {
-        return DEVICE_NOT_SUPPORTED;
-    }
+// int STM32SerialBLE::begin()
+// {
+//     if ((status & DEVICE_INITIALIZED) != 0) {
+//         return DEVICE_NOT_SUPPORTED;
+//     }
 
-    status |= DEVICE_INITIALIZED;
+//     status |= DEVICE_INITIALIZED;
 
-    serialService.addCharacteristic(rxSerialCharac);
-    serialService.addCharacteristic(txSerialCharac);
-    BLE.addService(serialService);
+//     serialService.addCharacteristic(rxSerialCharac);
+//     serialService.addCharacteristic(txSerialCharac);
+//     BLE.addService(serialService);
 
-    rxSerialCharac.setEventHandler(BLEWritten, [&](BLEDevice d, BLECharacteristic c) { rxReceivedData(c); });
-    return DEVICE_OK;
-}
+//     rxSerialCharac.setEventHandler(BLEWritten, [&](BLEDevice d, BLECharacteristic c) { rxReceivedData(c); });
+//     return DEVICE_OK;
+// }
 
-string STM32SerialBLE::readUntil(char delimiter)
-{
-    string result;
+// string STM32SerialBLE::readUntil(char delimiter)
+// {
+//     string result;
 
-    while (result.back() != delimiter && !rxBuffer.empty()) {
-        result += (char)rxBuffer.front();
-        rxBuffer.pop();
-    }
+//     while (result.back() != delimiter && !rxBuffer.empty()) {
+//         result += (char)rxBuffer.front();
+//         rxBuffer.pop();
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-vector<uint8_t> STM32SerialBLE::readBuffer(size_t nb)
-{
-    vector<uint8_t> result;
-    size_t byteToRead;
+// vector<uint8_t> STM32SerialBLE::readBuffer(size_t nb)
+// {
+//     vector<uint8_t> result;
+//     size_t byteToRead;
 
-    if (nb == 0) {
-        byteToRead = rxBuffer.size();
-    }
-    else {
-        byteToRead = std::min(nb, rxBuffer.size());
-    }
+//     if (nb == 0) {
+//         byteToRead = rxBuffer.size();
+//     }
+//     else {
+//         byteToRead = std::min(nb, rxBuffer.size());
+//     }
 
-    for (size_t i = 0; i < byteToRead; ++i) {
-        result.push_back(rxBuffer.front());
-        rxBuffer.pop();
-    }
+//     for (size_t i = 0; i < byteToRead; ++i) {
+//         result.push_back(rxBuffer.front());
+//         rxBuffer.pop();
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-void STM32SerialBLE::send(string str)
-{
-    if (str.size() > BLE_TX_BUFFER_SIZE) {
-        txSerialCharac.writeValue(str.substr(0, BLE_TX_BUFFER_SIZE));
-        send(str.substr(BLE_TX_BUFFER_SIZE + 1));
-    }
-    else {
-        txSerialCharac.writeValue(str);
-    }
-}
+// void STM32SerialBLE::send(string str)
+// {
+//     if (str.size() > BLE_TX_BUFFER_SIZE) {
+//         txSerialCharac.writeValue(str.substr(0, BLE_TX_BUFFER_SIZE));
+//         send(str.substr(BLE_TX_BUFFER_SIZE + 1));
+//     }
+//     else {
+//         txSerialCharac.writeValue(str);
+//     }
+// }
 
-void STM32SerialBLE::sendBuffer(uint8_t* buf, size_t len)
-{
-    string str((char*)buf, len);
-    send(str);
-}
+// void STM32SerialBLE::sendBuffer(uint8_t* buf, size_t len)
+// {
+//     string str((char*)buf, len);
+//     send(str);
+// }
 
-void STM32SerialBLE::rxReceivedData(BLECharacteristic& characteristic)
-{
-    auto value = characteristic.value();
-    for (int i = 0; i < characteristic.valueLength(); ++i) {
-        rxBuffer.push(value[i]);
-    }
+// void STM32SerialBLE::rxReceivedData(BLECharacteristic& characteristic)
+// {
+//     auto value = characteristic.value();
+//     for (int i = 0; i < characteristic.valueLength(); ++i) {
+//         rxBuffer.push(value[i]);
+//     }
 
-    if (count(value, value + characteristic.valueLength(), (uint8_t)charOnDelimiter) > 0) {
-        codal::Event(STM32_ID_BLE_SERIAL, STM32_BLE_EVT_DELIMITER);
-    }
-}
+//     if (count(value, value + characteristic.valueLength(), (uint8_t)charOnDelimiter) > 0) {
+//         codal::Event(STM32_ID_BLE_SERIAL, STM32_BLE_EVT_DELIMITER);
+//     }
+// }
--- source/STM32AdvertisingBLE.cpp
+++ source/STM32AdvertisingBLE.cpp
@@ -1,279 +1,280 @@
-#include "STM32AdvertisingBLE.h"
+// #include "STM32AdvertisingBLE.h"
 
-#include <vector>
+// #include <vector>
 
-#include "EventModel.h"
-#include "Timer.h"
-#include "codal_target_hal.h"
+// #include "EventModel.h"
+// #include "Timer.h"
+// #include "codal_target_hal.h"
 
-using namespace std;
-using namespace codal;
+// using namespace std;
+// using namespace codal;
 
-constexpr uint16_t EVENT_STATE_UPDATE   = 0x1415;  // 0x1415 = 5141 in decimal...\o/
-constexpr uint16_t EVENT_CLEAN_MESSAGES = 0x1135;  // Fibonacci time !
+// constexpr uint16_t EVENT_STATE_UPDATE   = 0x1415;  // 0x1415 = 5141 in decimal...\o/
+// constexpr uint16_t EVENT_CLEAN_MESSAGES = 0x1135;  // Fibonacci time !
 
-bool STM32AdvertisingBLE::isTimerSet = false;
+// bool STM32AdvertisingBLE::isTimerSet = false;
 
-STM32AdvertisingBLE::STM32AdvertisingBLE(uint8_t channel)
-    : state(OFF),
-      hasData(false),
-      isRunning(false),
-      channel(channel),
-      durationScan(5000),
-      durationEmit(5000),
-      retainingTime(BLE_DEFAULT_RETENTION_TIME),
-      lastStateChange(0),
-      localName("Default Name")
-{
-    messages.clear();
+// STM32AdvertisingBLE::STM32AdvertisingBLE(uint8_t channel)
+//     : state(OFF),
+//       hasData(false),
+//       isRunning(false),
+//       channel(channel),
+//       durationScan(5000),
+//       durationEmit(5000),
+//       retainingTime(BLE_DEFAULT_RETENTION_TIME),
+//       lastStateChange(0),
+//       localName("Default Name")
+// {
+//     messages.clear();
 
-    if (EventModel::defaultEventBus) {
-        if (!isTimerSet) {
-            system_timer_event_every(250, DEVICE_ID_RADIO, EVENT_STATE_UPDATE);
-            system_timer_event_every(BLE_MIN_RETENTION_TIME, DEVICE_ID_RADIO, EVENT_CLEAN_MESSAGES);
-            isTimerSet = true;
-        }
+//     if (EventModel::defaultEventBus) {
+//         if (!isTimerSet) {
+//             system_timer_event_every(250, DEVICE_ID_RADIO, EVENT_STATE_UPDATE);
+//             system_timer_event_every(BLE_MIN_RETENTION_TIME, DEVICE_ID_RADIO, EVENT_CLEAN_MESSAGES);
+//             isTimerSet = true;
+//         }
 
-        EventModel::defaultEventBus->listen(DEVICE_ID_RADIO, EVENT_STATE_UPDATE, this,
-                                            &STM32AdvertisingBLE::stateUpdate);
-        EventModel::defaultEventBus->listen(DEVICE_ID_RADIO, EVENT_CLEAN_MESSAGES, this,
-                                            &STM32AdvertisingBLE::cleanMessages);
-    }
-}
+//         EventModel::defaultEventBus->listen(DEVICE_ID_RADIO, EVENT_STATE_UPDATE, this,
+//                                             &STM32AdvertisingBLE::stateUpdate);
+//         EventModel::defaultEventBus->listen(DEVICE_ID_RADIO, EVENT_CLEAN_MESSAGES, this,
+//                                             &STM32AdvertisingBLE::cleanMessages);
+//     }
+// }
 
-void STM32AdvertisingBLE::begin()
-{
-    lastStateChange = 0;
-    if (hasData) {
-        enableAdvertising();
-    }
-    else {
-        enableScan();
-    }
+// void STM32AdvertisingBLE::begin()
+// {
+//     lastStateChange = 0;
+//     if (hasData) {
+//         enableAdvertising();
+//     }
+//     else {
+//         enableScan();
+//     }
 
-    isRunning = true;
-}
+//     isRunning = true;
+// }
 
-void STM32AdvertisingBLE::end()
-{
-    isRunning = false;
-    state     = OFF;
-    disableAdvertising();
-    disableScan();
-}
+// void STM32AdvertisingBLE::end()
+// {
+//     isRunning = false;
+//     state     = OFF;
+//     disableAdvertising();
+//     disableScan();
+// }
 
-void STM32AdvertisingBLE::setServiceData(uint16_t uuidService, uint8_t* data, size_t length)
-{
-    setData(uuidService, data, length, true);
-}
+// void STM32AdvertisingBLE::setServiceData(uint16_t uuidService, uint8_t* data, size_t length)
+// {
+//     setData(uuidService, data, length, true);
+// }
 
-void STM32AdvertisingBLE::clearData()
-{
-    advData.clear();
-    hasData = false;
-}
+// void STM32AdvertisingBLE::clearData()
+// {
+//     advData.clear();
+//     hasData = false;
+// }
 
-bool STM32AdvertisingBLE::hasReceivedMessage()
-{
-    for (auto it = messages.begin(); it != messages.end(); it++) {
-        if (!it->isRead) return true;
-    }
+// bool STM32AdvertisingBLE::hasReceivedMessage()
+// {
+//     for (auto it = messages.begin(); it != messages.end(); it++) {
+//         if (!it->isRead) return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-bool STM32AdvertisingBLE::hasReceivedMessageFrom(std::string name)
-{
-    for (auto msg : messages) {
-        if (msg.name == name && !msg.isRead) return true;
-    }
+// bool STM32AdvertisingBLE::hasReceivedMessageFrom(std::string name)
+// {
+//     for (auto msg : messages) {
+//         if (msg.name == name && !msg.isRead) return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-bool STM32AdvertisingBLE::hasReceivedMessageFrom(std::string name, std::string uuid)
-{
-    for (auto msg : messages) {
-        if (msg.name == name && msg.uuid == uuid && !msg.isRead) return true;
-    }
+// bool STM32AdvertisingBLE::hasReceivedMessageFrom(std::string name, std::string uuid)
+// {
+//     for (auto msg : messages) {
+//         if (msg.name == name && msg.uuid == uuid && !msg.isRead) return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-std::deque<ReceivedMessage> STM32AdvertisingBLE::getAllReceivedMessage()
-{
-    std::deque<ReceivedMessage> result;
+// std::deque<ReceivedMessage> STM32AdvertisingBLE::getAllReceivedMessage()
+// {
+//     std::deque<ReceivedMessage> result;
 
-    for (auto it = messages.begin(); it != messages.end(); it++) {
-        if (!it->isRead) {
-            result.push_back(ReceivedMessage(*it));
-            it->isRead = true;
-        }
-    }
+//     for (auto it = messages.begin(); it != messages.end(); it++) {
+//         if (!it->isRead) {
+//             result.push_back(ReceivedMessage(*it));
+//             it->isRead = true;
+//         }
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-std::deque<ReceivedMessage> STM32AdvertisingBLE::getAllReceivedMessageFrom(std::string name)
-{
-    std::deque<ReceivedMessage> result;
+// std::deque<ReceivedMessage> STM32AdvertisingBLE::getAllReceivedMessageFrom(std::string name)
+// {
+//     std::deque<ReceivedMessage> result;
 
-    for (auto it = messages.begin(); it != messages.end(); it++) {
-        if (it->name == name && !it->isRead) {
-            result.push_back(ReceivedMessage(*it));
-            it->isRead = true;
-        }
-    }
+//     for (auto it = messages.begin(); it != messages.end(); it++) {
+//         if (it->name == name && !it->isRead) {
+//             result.push_back(ReceivedMessage(*it));
+//             it->isRead = true;
+//         }
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-ReceivedMessage STM32AdvertisingBLE::getReceivedMessageFrom(std::string name, std::string uuid)
-{
-    for (auto it = messages.begin(); it != messages.end(); it++) {
-        if (it->name == name && it->uuid == uuid && !it->isRead) {
-            return ReceivedMessage(*it);
-            it->isRead = true;
-        }
-    }
+// ReceivedMessage STM32AdvertisingBLE::getReceivedMessageFrom(std::string name, std::string uuid)
+// {
+//     for (auto it = messages.begin(); it != messages.end(); it++) {
+//         if (it->name == name && it->uuid == uuid && !it->isRead) {
+//             return ReceivedMessage(*it);
+//             it->isRead = true;
+//         }
+//     }
 
-    return ReceivedMessage();
-}
+//     return ReceivedMessage();
+// }
 
-void STM32AdvertisingBLE::stateUpdate(Event)
-{
-    if (!isRunning) {
-        return;
-    }
+// void STM32AdvertisingBLE::stateUpdate(Event)
+// {
+//     if (!isRunning) {
+//         return;
+//     }
 
-    uint32_t delta = getCurrentMillis() - lastStateChange;
+//     uint32_t delta = getCurrentMillis() - lastStateChange;
 
-    switch (state) {
-        case EMIT:
-            if (delta >= durationEmit && durationScan > 0) {
-                enableScan();
-            }
-            break;
+//     switch (state) {
+//         case EMIT:
+//             if (delta >= durationEmit && durationScan > 0) {
+//                 enableScan();
+//             }
+//             break;
 
-        case SCAN:
-            if (delta >= durationScan && hasData && durationEmit > 0) {
-                saveScanResult();
-                enableAdvertising();
-            }
-            else {
-                saveScanResult();
-            }
-            break;
+//         case SCAN:
+//             if (delta >= durationScan && hasData && durationEmit > 0) {
+//                 saveScanResult();
+//                 enableAdvertising();
+//             }
+//             else {
+//                 saveScanResult();
+//             }
+//             break;
 
-        case OFF:
-        default:
-            break;
-    }
-}
+//         case OFF:
+//         default:
+//             break;
+//     }
+// }
 
-void STM32AdvertisingBLE::cleanMessages(Event)
-{
-    uint32_t currentTime = getCurrentMillis();
+// void STM32AdvertisingBLE::cleanMessages(Event)
+// {
+//     uint32_t currentTime = getCurrentMillis();
 
-    for (auto it = messages.begin(); it != messages.end(); it++) {
-        if (it->expirationTime <= currentTime) {
-            it = messages.erase(it);
-        }
-    }
-}
+//     for (auto it = messages.begin(); it != messages.end(); it++) {
+//         if (it->expirationTime <= currentTime) {
+//             it = messages.erase(it);
+//         }
+//     }
+// }
 
-void STM32AdvertisingBLE::setData(uint16_t uuid, uint8_t* data, size_t length, bool isService)
-{
-    size_t dataLength = length > MAX_ADVERTISING_DATA_LENGTH ? MAX_ADVERTISING_DATA_LENGTH : length;
+// void STM32AdvertisingBLE::setData(uint16_t uuid, uint8_t* data, size_t length, bool isService)
+// {
+//     size_t dataLength = length > MAX_ADVERTISING_DATA_LENGTH ? MAX_ADVERTISING_DATA_LENGTH : length;
 
-    if (isRunning && state == EMIT) {
-        BLE.stopAdvertise();
-    }
+//     if (isRunning && state == EMIT) {
+//         BLE.stopAdvertise();
+//     }
 
-    advData.clear();
-    if (isService) {
-        advData.setManufacturerData(NULL, 0);
-        advData.setAdvertisedServiceData(uuid, data, dataLength);
-    }
-    else {
-        advData.setAdvertisedServiceData(uuid, NULL, 0);
-        advData.setManufacturerData(uuid, data, dataLength);
-    }
+//     advData.clear();
+//     if (isService) {
+//         advData.setManufacturerData(NULL, 0);
+//         advData.setAdvertisedServiceData(uuid, data, dataLength);
+//     }
+//     else {
+//         advData.setAdvertisedServiceData(uuid, NULL, 0);
+//         advData.setManufacturerData(uuid, data, dataLength);
+//     }
 
-    BLE.setAdvertisingData(advData);
+//     BLE.setAdvertisingData(advData);
 
-    if (isRunning && state == EMIT) {
-        BLE.advertise(channel);
-    }
+//     if (isRunning && state == EMIT) {
+//         BLE.advertise(channel);
+//     }
 
-    hasData = true;
-}
+//     hasData = true;
+// }
 
-void STM32AdvertisingBLE::enableScan()
-{
-    disableAdvertising();
-    BLE.scan(true);
+// void STM32AdvertisingBLE::enableScan()
+// {
+//     disableAdvertising();
+//     BLE.scan(true);
 
-    state           = SCAN;
-    lastStateChange = getCurrentMillis() + (target_random(101) - 50);  // Add random to avoid synchronous symptoms
-}
+//     state           = SCAN;
+//     lastStateChange = getCurrentMillis() + (target_random(101) - 50);  // Add random to avoid synchronous symptoms
+// }
 
-void STM32AdvertisingBLE::disableScan()
-{
-    BLE.stopScan();
-}
+// void STM32AdvertisingBLE::disableScan()
+// {
+//     BLE.stopScan();
+// }
 
-void STM32AdvertisingBLE::enableAdvertising()
-{
-    disableScan();
+// void STM32AdvertisingBLE::enableAdvertising()
+// {
+//     disableScan();
 
-    BLE.setLocalName(localName);
-    BLE.setConnectable(false);
-    BLE.setAdvertisingData(advData);
-    BLE.advertise(channel);
+//     BLE.setLocalName(localName);
+//     BLE.setConnectable(false);
+//     BLE.setAdvertisingData(advData);
+//     BLE.advertise(channel);
 
-    state           = EMIT;
-    lastStateChange = getCurrentMillis() + (target_random(101) - 50);  // Add random to avoid synchronous symptoms
-}
+//     state           = EMIT;
+//     lastStateChange = getCurrentMillis() + (target_random(101) - 50);  // Add random to avoid synchronous symptoms
+// }
 
-void STM32AdvertisingBLE::disableAdvertising()
-{
-    BLE.stopAdvertise();
-}
+// void STM32AdvertisingBLE::disableAdvertising()
+// {
+//     BLE.stopAdvertise();
+// }
 
-void STM32AdvertisingBLE::saveScanResult()
-{
-    bool addMessage = true;
-    std::string tmpUuid;
-    std::vector<uint8_t> tmpMsg;
+// void STM32AdvertisingBLE::saveScanResult()
+// {
+//     bool addMessage = true;
+//     std::string tmpUuid;
+//     std::vector<uint8_t> tmpMsg;
 
-    for (BLEDevice device = BLE.available(); device; device = BLE.available()) {
-        for (auto i = 0; i < device.advertisingDataCount(); i++) {
-            addMessage = true;
-            tmpUuid    = device.getAdvertisingDataUuid(i);
+//     for (BLEDevice device = BLE.available(); device; device = BLE.available()) {
+//         for (auto i = 0; i < device.advertisingDataCount(); i++) {
+//             addMessage = true;
+//             tmpUuid    = device.getAdvertisingDataUuid(i);
 
-            for (auto it = messages.begin(); it != messages.end(); it++) {
-                if (it->address == device.address() && it->uuid == tmpUuid) {
-                    tmpMsg = device.getAdvertisingData(i);
+//             for (auto it = messages.begin(); it != messages.end(); it++) {
+//                 if (it->address == device.address() && it->uuid == tmpUuid) {
+//                     tmpMsg = device.getAdvertisingData(i);
 
-                    if (it->message != tmpMsg) {
-                        it->name           = device.hasLocalName() ? device.localName() : "N/A";
-                        it->rssi           = device.rssi();
-                        it->message        = tmpMsg;
-                        it->isRead         = false;
-                        it->expirationTime = getCurrentMillis() + retainingTime;
-                    }
+//                     if (it->message != tmpMsg) {
+//                         it->name           = device.hasLocalName() ? device.localName() : "N/A";
+//                         it->rssi           = device.rssi();
+//                         it->message        = tmpMsg;
+//                         it->isRead         = false;
+//                         it->expirationTime = getCurrentMillis() + retainingTime;
+//                     }
 
-                    addMessage = false;
-                    break;
-                }
-            }
+//                     addMessage = false;
+//                     break;
+//                 }
+//             }
 
-            if (addMessage) {
-                messages.push_back(ReceivedMessage(device.address(), device.hasLocalName() ? device.localName() : "N/A",
-                                                   tmpUuid.c_str(), device.getAdvertisingData(i), device.rssi(),
-                                                   retainingTime));
-            }
-        }
-    }
-}
+//             if (addMessage) {
+//                 messages.push_back(ReceivedMessage(device.address(), device.hasLocalName() ? device.localName() :
+//                 "N/A",
+//                                                    tmpUuid.c_str(), device.getAdvertisingData(i), device.rssi(),
+//                                                    retainingTime));
+//             }
+//         }
+//     }
+// }
