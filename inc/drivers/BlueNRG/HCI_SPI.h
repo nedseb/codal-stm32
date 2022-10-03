a6ddd610b6022cfb9f50f4161e3bb42df856b1da
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-10-03 14:16:07 +0000

    Implement Advertising function

--- inc/drivers/BlueNRG/HCI_SPI.h
+++ inc/drivers/BlueNRG/HCI_SPI.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "CodalFiber.h"
 #include "HCI.h"
 #include "STM32Pin.h"
 #include "STM32SPI.h"
@@ -9,7 +10,7 @@
 class HCI_SPI : public HCI {
   public:
     HCI_SPI(codal::STM32SPI& spi, codal::STM32Pin& cs, codal::STM32Pin& irq, codal::STM32Pin& rst,
-            uint32_t freq = 8000000);
+            uint32_t freq = 1000000);
 
     virtual ~HCI_SPI() {}
 
@@ -22,7 +23,7 @@ class HCI_SPI : public HCI {
     virtual uint8_t available() override final;
     virtual uint8_t readByte() override final;
     virtual void readBytes(uint8_t* data, uint8_t size) override final;
-    virtual void writeBytes(uint8_t* data, uint8_t size) override final;
+    virtual bool writeBytes(uint8_t* data, uint8_t size) override final;
 
   private:
     codal::STM32SPI& spi;
@@ -33,10 +34,12 @@ class HCI_SPI : public HCI {
     void enableCS()
     {
         cs.setDigitalValue(0);
-        target_wait_us(500);
+        target_wait_us(100);
     }
-    void disableCS() { cs.setDigitalValue(1); }
+    void disableCS()
+    {
+        cs.setDigitalValue(1);
+        target_wait_us(100);
+    }
     void waitForInit();
-
-    bool isReadyToWrite(uint8_t nbByteToWrite);
 };
