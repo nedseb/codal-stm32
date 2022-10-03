1ed3b136db2b4f71699f548ff3e56fc2bf12af5d
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-10-03 14:11:56 +0000

    Finish base implementation and add advertissing functions

--- source/drivers/BlueNRG/HCI.cpp
+++ source/drivers/BlueNRG/HCI.cpp
@@ -1,7 +1,13 @@
 #include "HCI.h"
 
+#include <cstring>
+#include <queue>
+
+#include "CodalFiber.h"
+#include "ble_utils.h"
 #include "clock.h"
 
+constexpr uint16_t MAX_BUFFER_POLL_SIZE  = 256;
 constexpr uint32_t EXPIRATION_PACKETS_MS = 10000;
 constexpr uint32_t TIMEOUT_COMMAND_MS    = 1000;
 
@@ -16,37 +22,57 @@ constexpr uint8_t HCI_EVENT_PKT     = 0x04;
 
 void HCI::poll()
 {
-    for (uint8_t nbBytesToRead = available(); nbBytesToRead > 0; nbBytesToRead = available()) {
-        uint8_t typeByte = readByte();
+    std::queue<uint8_t> buffer;
 
-        if (typeByte == HCI_EVENT_PKT) {
-            EventPacket pkt;
-            pkt.eventCode = readByte();
-            pkt.length    = readByte();
+    for (uint8_t nbBytes = available(); nbBytes > 0 && buffer.size() < MAX_BUFFER_POLL_SIZE; nbBytes = available()) {
+        if (isDebug) printf("[poll] Available bytes : %d\r\n", nbBytes);
 
-            pkt.params.resize(pkt.length);
-            readBytes(pkt.params.data(), pkt.length);
+        uint8_t tmpBuf[nbBytes] = {0x00};
+        readBytes(tmpBuf, nbBytes);
 
-            pkt.time = getCurrentMillis();
+        for (uint8_t i = 0; i < nbBytes; ++i) buffer.push(tmpBuf[i]);
+    }
 
-            eventPackets.push_front(pkt);
+    while (!buffer.empty()) {
+        uint8_t typeByte = buffer.front();
+        buffer.pop();
 
-            if (isDebug) {
-                printf("[poll] received event : [0x%0.2x] - %d bytes\r\n", pkt.eventCode, pkt.params.size());
-                printf("\t");
-                for (auto v : pkt.params) printf("0x%0.2x ", v);
-                printf("\r\n");
-            }
-        }
-        else if (typeByte == HCI_ASYNCDATA_PKT) {
-            if (isDebug) printf("[poll] received data : not implemented\r\n");
-            // if (isDebug) printf("[poll] received data : [%d] - %d bytes", pkt.eventCode, pkt.length);
-        }
-        else {
-            // Delete message ?
-            if (isDebug) printf("[poll] received unknown message type : 0x%0.2X\r\n", typeByte);
+        switch (typeByte) {
+            case HCI_EVENT_PKT: {
+                EventPacket pkt;
+                pkt.eventCode = buffer.front();
+                buffer.pop();
+                pkt.length = buffer.front();
+                buffer.pop();
+
+                pkt.params.clear();
+                for (uint8_t i = 0; i < pkt.length; ++i) {
+                    pkt.params.push_back(buffer.front());
+                    buffer.pop();
+                }
+
+                pkt.time = getCurrentMillis();
+                eventPackets.push_front(pkt);
+
+                if (isDebug) {
+                    printf("[poll] received event : [0x%0.2x] - %d bytes\r\n", pkt.eventCode, pkt.params.size());
+                    printf("\tParams: ");
+                    for (auto v : pkt.params) printf("0x%0.2x ", v);
+                    printf("\r\n");
+                }
+            } break;
+
+            case HCI_ASYNCDATA_PKT:
+                if (isDebug) printf("[poll] received data : not implemented\r\n");
+                break;
+
+            default:
+                if (isDebug) printf("[poll] unknown message type : 0x%0.2X\r\n", typeByte);
+                break;
         }
     }
+
+    if (isDebug) printf("[poll] No data availables....\r\n\r\n");
 
     cleanPackets();
 }
@@ -109,29 +135,69 @@ bool HCI::leSetAdvertisingParameters(uint16_t advertisingIntervalMin, uint16_t a
                                      PeerAddressType peerAddressType, uint64_t peerAddress,
                                      uint8_t advertisingChannelMap, AdvertisingFilterPolicy advertisingFilterPolicy)
 {
-    return false;
+    uint8_t data[15] = {
+        BLE_Utils::getLsb(advertisingIntervalMin),
+        BLE_Utils::getMsb(advertisingIntervalMin),
+
+        BLE_Utils::getLsb(advertisingIntervalMax),
+        BLE_Utils::getMsb(advertisingIntervalMax),
+
+        (uint8_t)advertisingType,
+        (uint8_t)ownAddressType,
+        (uint8_t)peerAddressType,
+
+        BLE_Utils::getByte(peerAddress, 0),
+        BLE_Utils::getByte(peerAddress, 1),
+        BLE_Utils::getByte(peerAddress, 2),
+        BLE_Utils::getByte(peerAddress, 3),
+        BLE_Utils::getByte(peerAddress, 4),
+        BLE_Utils::getByte(peerAddress, 5),
+
+        advertisingChannelMap,
+
+        (uint8_t)advertisingFilterPolicy,
+    };
+    auto result = sendCommand(OpCodeCommand::LE_SET_ADV_PARAM, 15, data);
+
+    return !result.empty() && result.size() == 1 && result[0] == 0x00;
 }
 
-uint8_t HCI::leReadAdvertisingPhysicalChannelTxPower()
+int8_t HCI::leReadAdvertisingPhysicalChannelTxPower()
 {
-    return 0;
+    auto result = sendCommand(OpCodeCommand::LE_READ_TX_POWER);
+
+    if (result.empty() || result.size() != 2 || result[0] != 0x00) {
+        return 127;
+    }
+
+    return (int8_t)result[1];
 }
 
-bool HCI::leSetAdvertisingData(uint8_t length, uint8_t* data)
+bool HCI::leSetAdvertisingData(uint8_t significantSize, uint8_t* data)
 {
-    return false;
+    uint8_t tmp[32] = {significantSize, 0x00};
+    memcpy(tmp + 1, data, significantSize);
+
+    auto result = sendCommand(OpCodeCommand::LE_SET_ADV_DATA, 32, tmp);
+
+    return !result.empty() && result.size() == 1 && result[0] == 0x00;
 }
 
-bool HCI::leSetScanResponseData(uint8_t length, uint8_t* data)
+bool HCI::leSetScanResponseData(uint8_t significantSize, uint8_t* data)
 {
-    return false;
+    uint8_t tmp[32] = {significantSize, 0x00};
+    memcpy(tmp + 1, data, significantSize);
+
+    auto result = sendCommand(OpCodeCommand::LE_SET_SCAN_RESP_DATA, 32, tmp);
+
+    return !result.empty() && result.size() == 1 && result[0] == 0x00;
 }
 
 bool HCI::leSetAdvertisingEnable(bool enable)
 {
     uint8_t data[1] = {enable ? (uint8_t)0x01 : (uint8_t)0x00};
     auto result     = sendCommand(OpCodeCommand::LE_SET_ADV_ENABLE, 1, data);
-    return !result.empty() && result.front() > 0;
+    return !result.empty() && result.front() == 0x00;
 }
 
 void HCI::cleanPackets()
