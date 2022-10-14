49a6c8aa83a068f373e3fa4c0abf72e305b0cc34
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-10-14 09:45:14 +0000

    Create class to represent a remote BLE Device (ex, scan result)

---
+++ source/drivers/BlueNRG/BLERemoteDevice.cpp
@@ -0,0 +1,43 @@
+#include "BLERemoteDevice.h"
+
+std::vector<BLERemoteDevice> BLERemoteDevice::buildFromAdvertisingReports(std::deque<BLEAdvertisingReport> reports)
+{
+    std::vector<BLERemoteDevice> devices;
+    size_t idx;
+
+    for (BLEAdvertisingReport report : reports) {
+        idx = isReportInVector(devices, report);
+
+        if (idx < devices.size()) {
+            if (!report.name.empty()) {
+                devices[idx].name = report.name;
+            }
+            devices[idx].rssi = report.rssi;
+
+            for (auto sd : report.serviceData) {
+                devices[idx].serviceDatas.push_back(sd);
+            }
+        }
+        else {
+            BLERemoteDevice dev;
+            dev.address      = report.address;
+            dev.name         = report.name;
+            dev.rssi         = report.rssi;
+            dev.serviceDatas = report.serviceData;
+            devices.push_back(dev);
+        }
+    }
+
+    return devices;
+}
+
+size_t BLERemoteDevice::isReportInVector(std::vector<BLERemoteDevice>& devices, BLEAdvertisingReport& report)
+{
+    for (size_t i = 0; i < devices.size(); ++i) {
+        if (devices[i].address == report.address) {
+            return i;
+        }
+    }
+
+    return devices.size();
+}
