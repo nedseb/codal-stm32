1ac197478b7fff231e24af24849a77e941d037b1
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-10-14 09:37:50 +0000

    Add LE Event Code

--- inc/drivers/BlueNRG/utils/events_code.h
+++ inc/drivers/BlueNRG/utils/events_code.h
@@ -3,3 +3,16 @@
 #include <cstdint>
 
 constexpr uint8_t EVT_COMMAND_COMPLETE = 0x0E;
+constexpr uint8_t LE_EVENT_CODE        = 0x3E;
+
+constexpr uint8_t LE_EVENT_CONNECTION_COMPLETE                 = 0x01;
+constexpr uint8_t LE_EVENT_ADVERTISING_REPORT                  = 0x02;
+constexpr uint8_t LE_EVENT_CONNECTION_UPDATE_COMPLETE          = 0x03;
+constexpr uint8_t LE_EVENT_READ_REMOTE_USED_FEATURE_COMPLETE   = 0x04;
+constexpr uint8_t LE_EVENT_LONG_TERM_KEY_REQUESTED             = 0x05;
+constexpr uint8_t LE_EVENT_REMOTE_CONN_PARAM_REQUEST           = 0x06;
+constexpr uint8_t LE_EVENT_DATA_LENGTH_CHANGE                  = 0x07;
+constexpr uint8_t LE_EVENT_READ_LOCAL_P256_PUBLIC_KEY_COMPLETE = 0x08;
+constexpr uint8_t LE_EVENT_DHKEY_COMPLETE                      = 0x09;
+constexpr uint8_t LE_EVENT_ENHANCED_CONNECTION_COMPLETE        = 0x0A;
+constexpr uint8_t LE_EVENT_DIRECT_ADVERTISING_REPORT           = 0x0B;
