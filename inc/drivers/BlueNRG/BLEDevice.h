7ab64165dd6c0e235beabcd4c667946692a84198
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-10-14 09:45:35 +0000

    Add scan functions

--- inc/drivers/BlueNRG/BLEDevice.h
+++ inc/drivers/BlueNRG/BLEDevice.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "AdvertisingData.h"
+#include "BLERemoteDevice.h"
 #include "HCI.h"
 
 enum class BLEDeviceError : uint8_t {
@@ -12,6 +13,8 @@ enum class BLEDeviceError : uint8_t {
     LE_SET_ADVERTISING_DATA_ERROR      = 0x05,
     LE_SET_SCAN_RESPONSE_DATA_ERROR    = 0x06,
     LE_SET_ADVERTISING_ENABLE_ERROR    = 0x07,
+    LE_SET_SCAN_PARAMS_ERROR           = 0x08,
+    LE_SET_SCAN_ENABLE_ERROR           = 0x09,
 
     ALREADY_ADVERTISING_ERROR = 0xF0,
     ALREADY_SCANNING_ERROR    = 0xF1,
@@ -22,7 +25,8 @@ class BLEDevice {
     BLEDevice(HCI* hci);
     ~BLEDevice();
 
-    void init();
+    bool init();
+    void poll();
 
     void setAdvertisingData(AdvertisingData* data);
     void setScanResponseData(AdvertisingData* scan);
@@ -32,10 +36,23 @@ class BLEDevice {
     BLEDeviceError updateScanResponseData();
     BLEDeviceError stopAdvertising();
 
+    BLEDeviceError startScanning();
+    BLEDeviceError stopScanning();
+
+    size_t availableScan() { return remoteDevice.size(); }
+    size_t availableScan(std::string deviceName);
+
+    std::vector<BLERemoteDevice> getScanResult();
+    std::vector<BLERemoteDevice> getScanResult(std::string deviceName);
+
   private:
     HCI* hci;
     AdvertisingData* advData;
     AdvertisingData* advScanData;
+    std::list<BLERemoteDevice> remoteDevice;
 
     bool isAdvertising;
+    bool isScanning;
+
+    void handleAdvReport(std::deque<BLEAdvertisingReport> reports);
 };
--- source/drivers/BlueNRG/BLEDevice.cpp
+++ source/drivers/BlueNRG/BLEDevice.cpp
@@ -1,12 +1,43 @@
 #include "BLEDevice.h"
 
-BLEDevice::BLEDevice(HCI* hci) : hci(hci), advData(nullptr), advScanData(nullptr), isAdvertising(false) {}
+BLEDevice::BLEDevice(HCI* hci)
+    : hci(hci), advData(nullptr), advScanData(nullptr), isAdvertising(false), isScanning(false)
+{
+}
 
 BLEDevice::~BLEDevice() {}
 
-void BLEDevice::init()
+bool BLEDevice::init()
 {
-    hci->init();
+    if (!hci->init()) {
+        return false;
+    }
+
+    auto maskBT = EventMaskBuilder().setDefault().setLEMeta();
+    auto maskLE = LeEventMaskBuilder().setDefault();
+
+    if (hci->isDebugEnable()) printf("[init] Set Event Mask Bluetooth\r\n");
+    if (!hci->setEventMask(maskBT.getMSBMask(), maskBT.getLSBMask())) {
+        if (hci->isDebugEnable()) printf("[init] ACI GAP Init FAILEDr\n");
+        return false;
+    }
+
+    if (hci->isDebugEnable()) printf("[init] Set Event Mask BLE\r\n");
+    if (!hci->leSetEventMask(maskLE.getMSBMask(), maskLE.getLSBMask())) {
+        if (hci->isDebugEnable()) printf("[init] ACI GAP Init FAILEDr\n");
+        return false;
+    }
+
+    return true;
+}
+
+void BLEDevice::poll()
+{
+    hci->poll();
+
+    if (hci->availableAdvReport() > 0) {
+        handleAdvReport(hci->getAdvReports());
+    }
 }
 
 void BLEDevice::setAdvertisingData(AdvertisingData* data)
@@ -25,7 +56,8 @@ BLEDeviceError BLEDevice::startAdvertising()
         return BLEDeviceError::ALREADY_ADVERTISING_ERROR;
     }
 
-    if (!hci->leSetAdvertisingParameters()) return BLEDeviceError::LE_SET_ADVERTISING_PARAMS_ERROR;
+    if (!hci->leSetAdvertisingParameters(1280.0f, 1280.0f, AdvertisingType::ADV_IND, OwnAddressType::RANDOM))
+        return BLEDeviceError::LE_SET_ADVERTISING_PARAMS_ERROR;
 
     if (hci->leReadAdvertisingPhysicalChannelTxPower() == 127)
         return BLEDeviceError::LE_READ_ADVERTISING_TX_POWER_ERROR;
@@ -64,4 +96,80 @@ BLEDeviceError BLEDevice::stopAdvertising()
 
     isAdvertising = false;
     return BLEDeviceError::SUCCESS;
+}
+
+BLEDeviceError BLEDevice::startScanning()
+{
+    if (isScanning) {
+        return BLEDeviceError::ALREADY_SCANNING_ERROR;
+    }
+
+    if (!hci->leSetScanParameters(ScanType::ACTIVE, 10.0f, 10.0f, OwnAddressType::RANDOM,
+                                  ScanningFilterPolicy::BASIC_UNFILTERED))
+        return BLEDeviceError::LE_SET_SCAN_PARAMS_ERROR;
+
+    if (!hci->leSetScanEnable(true)) return BLEDeviceError::LE_SET_SCAN_ENABLE_ERROR;
+
+    isScanning = true;
+    return BLEDeviceError::SUCCESS;
+}
+
+BLEDeviceError BLEDevice::stopScanning()
+{
+    if (!hci->leSetScanEnable(false)) return BLEDeviceError::LE_SET_SCAN_ENABLE_ERROR;
+
+    isScanning = false;
+    return BLEDeviceError::SUCCESS;
+}
+
+void BLEDevice::handleAdvReport(std::deque<BLEAdvertisingReport> reports)
+{
+    auto devices = BLERemoteDevice::buildFromAdvertisingReports(reports);
+
+    for (auto newDevice : devices) {
+        for (auto it = remoteDevice.begin(); it != remoteDevice.end(); ++it) {
+            if (it->getAddress() == newDevice.getAddress()) {
+                remoteDevice.erase(it);
+                break;
+            }
+        }
+
+        remoteDevice.push_back(newDevice);
+    }
+}
+
+size_t BLEDevice::availableScan(std::string deviceName)
+{
+    size_t result = 0;
+
+    for (auto d : remoteDevice) {
+        if (d.getName() == deviceName) result++;
+    }
+
+    return result;
+}
+
+std::vector<BLERemoteDevice> BLEDevice::getScanResult()
+{
+    auto result = std::vector<BLERemoteDevice>(remoteDevice.begin(), remoteDevice.end());
+    remoteDevice.clear();
+    return result;
+}
+
+std::vector<BLERemoteDevice> BLEDevice::getScanResult(std::string deviceName)
+{
+    std::vector<BLERemoteDevice> result;
+    auto it = remoteDevice.begin();
+
+    while (it != remoteDevice.end()) {
+        if (it->getName() == deviceName) {
+            result.push_back(*it);
+            it = remoteDevice.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
+
+    return result;
 }
