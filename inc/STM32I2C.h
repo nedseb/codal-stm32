3edde67f468bdd2f75166facd99dbfc6ba8a4413
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-07-18 16:09:11 +0000

    Add isDeviceAvailable function

--- inc/STM32I2C.h
+++ inc/STM32I2C.h
@@ -71,6 +71,14 @@ class STM32I2C : public codal::I2C {
     void endTransmission(bool sendStop = true);
 
     /**
+     * @brief Check if the deveice at address `address` is available
+     *
+     * @param address the address to test
+     * @return boolean
+     */
+    bool isDeviceAvailable(uint8_t address);
+
+    /**
      * @brief Write single byte to TX buffer.
      *
      * @param data byte to write to buffer
