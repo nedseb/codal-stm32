482c0eb8229b7f9ae758154a3f823f1eafcb6f88
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-08-07 09:22:23 +0000

    Setting up clocks

--- variants/STM32WBxx/WB55R(C-E-G)V/variant_STEAM32_WB55RG.cpp
+++ variants/STM32WBxx/WB55R(C-E-G)V/variant_STEAM32_WB55RG.cpp
@@ -80,11 +80,17 @@ WEAK void SystemClock_Config(void)
     RCC_OscInitStruct.OscillatorType =
         RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
     RCC_OscInitStruct.HSEState            = RCC_HSE_ON;
+    RCC_OscInitStruct.LSEState            = RCC_LSE_ON;
     RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
     RCC_OscInitStruct.HSI48State          = RCC_HSI48_ON;
-    RCC_OscInitStruct.LSEState            = RCC_LSE_ON;
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_NONE;
+    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM            = RCC_PLLM_DIV1;
+    RCC_OscInitStruct.PLL.PLLN            = 8;
+    RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLR            = RCC_PLLR_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ            = RCC_PLLQ_DIV2;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
         Error_Handler();
     }
