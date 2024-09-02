78668dcde618d144fdca97f5c862caf71083384a
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-09-02 09:26:03 +0000

    update variant for STM32G4xx family

--- variants/STM32G4xx/G471C(C-E)U/PinNamesVar.h
+++ variants/STM32G4xx/G471C(C-E)U/PinNamesVar.h
@@ -1,11 +1,8 @@
 /* Alternate pin name */
 PA_0_ALT1  = PA_0  | ALT1,
 PA_1_ALT1  = PA_1  | ALT1,
-PA_1_ALT2  = PA_1  | ALT2,
 PA_2_ALT1  = PA_2  | ALT1,
-PA_2_ALT2  = PA_2  | ALT2,
 PA_3_ALT1  = PA_3  | ALT1,
-PA_3_ALT2  = PA_3  | ALT2,
 PA_4_ALT1  = PA_4  | ALT1,
 PA_6_ALT1  = PA_6  | ALT1,
 PA_7_ALT1  = PA_7  | ALT1,
@@ -43,6 +40,8 @@ PB_14_ALT1 = PB_14 | ALT1,
 PB_15_ALT1 = PB_15 | ALT1,
 PB_15_ALT2 = PB_15 | ALT2,
 PC_6_ALT1  = PC_6  | ALT1,
+PC_10_ALT1 = PC_10 | ALT1,
+PC_11_ALT1 = PC_11 | ALT1,
 PC_13_ALT1 = PC_13 | ALT1,
 
 /* SYS_WKUP */
--- variants/STM32G4xx/G473C(B-C-E)T_G474C(B-C-E)T_G483CET_G484CET/PeripheralPins.c
+++ variants/STM32G4xx/G473C(B-C-E)T_G474C(B-C-E)T_G483CET_G484CET/PeripheralPins.c
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (c) 2020-2021, STMicroelectronics
+ * Copyright (c) 2020, STMicroelectronics
  * All rights reserved.
  *
  * This software component is licensed by ST under BSD 3-Clause license,
@@ -13,7 +13,7 @@
 /*
  * Automatically generated from STM32G473C(B-C-E)Tx.xml, STM32G474C(B-C-E)Tx.xml
  * STM32G483CETx.xml, STM32G484CETx.xml
- * CubeMX DB release 6.0.21
+ * CubeMX DB release 6.0.120
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
@@ -103,6 +103,8 @@ WEAK const PinMap PinMap_I2C_SCL[] = {
   {NC,         NP,   0}
 };
 #endif
+
+//*** No I3C ***
 
 //*** TIM ***
 
