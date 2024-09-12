65d6b5f1f475dd15549276a8fe2f4a8ce817cd34
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-09-12 12:28:55 +0000

    Update HAL wrapper

--- source/stm32/HAL/stm32yyxx_hal_lcd.c
+++ source/stm32/HAL/stm32yyxx_hal_lcd.c
@@ -1,12 +1,16 @@
+/* HAL raised several warnings, ignore them */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
 #ifdef STM32L0xx
-#include "stm32l0xx_hal_lcd.c"
+  #include "stm32l0xx_hal_lcd.c"
+#elif STM32L1xx
+  #include "stm32l1xx_hal_lcd.c"
+#elif STM32L4xx
+  #include "stm32l4xx_hal_lcd.c"
+#elif STM32U0xx
+  #include "stm32u0xx_hal_lcd.c"
+#elif STM32WBxx
+  #include "stm32wbxx_hal_lcd.c"
 #endif
-#ifdef STM32L1xx
-#include "stm32l1xx_hal_lcd.c"
-#endif
-#ifdef STM32L4xx
-#include "stm32l4xx_hal_lcd.c"
-#endif
-#ifdef STM32WBxx
-#include "stm32wbxx_hal_lcd.c"
-#endif
+#pragma GCC diagnostic pop
