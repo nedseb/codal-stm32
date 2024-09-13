f7bcc220a1ef2dbc7734e00634aab71411af4607
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-09-13 12:15:23 +0000

    Improve Hardfault handler to ease debug

--- source/STM32InterruptHandlers.c
+++ source/STM32InterruptHandlers.c
@@ -5,7 +5,56 @@
 /******************************************************************************/
 /*            Cortex-M4 Processor Interruption and Exception Handlers         */
 /******************************************************************************/
+typedef struct __attribute__((packed)) ContextStateFrame {
+    uint32_t r0;
+    uint32_t r1;
+    uint32_t r2;
+    uint32_t r3;
+    uint32_t r12;
+    uint32_t lr;
+    uint32_t return_address;
+    uint32_t xpsr;
+} sContextStateFrame;
+
+// NOTE: If you are using CMSIS, the registers can also be
+// accessed through CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk
+#define HALT_IF_DEBUGGING()                                 \
+    do {                                                    \
+        if ((*(volatile uint32_t*)0xE000EDF0) & (1 << 0)) { \
+            __asm("bkpt 1");                                \
+        }                                                   \
+    } while (0)
+
+#define HARDFAULT_HANDLING_ASM(_x) \
+    __asm volatile(                \
+        "tst lr, #4 \n"            \
+        "ite eq \n"                \
+        "mrseq r0, msp \n"         \
+        "mrsne r0, psp \n"         \
+        "b hardfault_handler_c \n")
+
+// Disable optimizations for this function so "frame" argument
+// does not get optimized away
+__attribute__((optimize("O0"))) void hardfault_handler_c(sContextStateFrame* frame)
+{
+    // If and only if a debugger is attached, execute a breakpoint
+    // instruction so we can take a look at what triggered the fault
+    HALT_IF_DEBUGGING();
+
+    // Logic for dealing with the exception. Typically:
+    //  - log the fault which occurred for postmortem analysis
+    //  - If the fault is recoverable,
+    //    - clear errors and return back to Thread Mode
+    //  - else
+    //    - reboot system
+}
+
 void init_Handlers(void) {}
+
+void HardFault_Handler()
+{
+    HARDFAULT_HANDLING_ASM();
+}
 
 #define UNWEAK(x)   \
     void x(void)    \
@@ -14,7 +63,7 @@ void init_Handlers(void) {}
         }           \
     }
 
-UNWEAK(HardFault_Handler)
+// UNWEAK(HardFault_Handler)
 UNWEAK(MemManage_Handler)
 UNWEAK(BusFault_Handler)
 UNWEAK(UsageFault_Handler)
